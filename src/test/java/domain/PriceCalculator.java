78bf8e7d4e2eb611bfafed157f7e4302212b326d
hanmogo <russell1677@naver.com>
2025-09-18 15:47:56 +0900

    feat:예매조건에 따른 할인 기능 구현 테스트

--- src/test/java/domain/PriceCalculator.java
+++ src/test/java/domain/PriceCalculator.java
@@ -1,15 +1,49 @@
 package domain;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 public class PriceCalculator {
 
+    //좌석 값 계산
     public int calculatePrice(List<Seat> seats) {
         int totalPrice = 0;
         for (Seat seat : seats) {
             totalPrice += seat.getPrice();
         }
         return totalPrice;
+    }
+
+
+    //무비데이 + 시간조건 할인 적용
+    public int applyDiscounts(int basePrice, Screening screening) {
+        int priceAfterMovieDay = applyMovieDayDiscount(basePrice, screening);
+        int finalPrice = applyTimeDiscount(priceAfterMovieDay, screening);
+        return finalPrice;
+    }
+
+
+    //무비데이 할인 적용
+    public int applyMovieDayDiscount(int basePrice, Screening movingDayScreening) {
+
+        int dayOfMonth = movingDayScreening.getStartTime().getDayOfMonth();
+
+        if (dayOfMonth == 10 || dayOfMonth == 20 || dayOfMonth == 30) {
+            return (int) (basePrice * 0.9);
+        }
+        return basePrice;
+    }
+
+
+    //시간 조건 할인
+    public int applyTimeDiscount(int basePrice, Screening timeScreening) {
+
+        LocalTime startTime = timeScreening.getStartTime().toLocalTime();
+        if(startTime.isBefore(LocalTime.of(11, 0))||startTime.isAfter(LocalTime.of(20, 0))) {
+            return basePrice - 2000;
+        }
+        return basePrice;
     }
 }
 
--- src/test/java/domain/PriceCalculatorTest.java
+++ src/test/java/domain/PriceCalculatorTest.java
@@ -4,6 +4,8 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.net.Authenticator;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PriceCalculatorTest {
 
     @Test
-    @DisplayName("선택한 좌석 목록의 기본 총액을 정확하게 계산함")
+    @DisplayName("선택한 좌석 목록의 기본 총액을 정확하게 계산한다.")
     void calculatePrice(){
         //given
         Seat seat1 = new Seat(1L, 1L, "A" ,1, Seat.SeatGrade.S);
@@ -29,6 +31,64 @@ class PriceCalculatorTest {
         //then 겁증
         assertThat(actualPrice).isEqualTo(expectedPrice);
 
+    }
+
+    @Test
+    @DisplayName("무비데이(10일, 20일, 30일)에는 총액에서 10% 할인이 적용된다.")
+    void applyMovieDayDiscount(){
+        //given
+        int basePrice = 20000;
+        //상영날짜 10일로 설정한 객체 생성
+        LocalDateTime screeningDateTime = LocalDateTime.of(2025, 9, 10, 14, 30);
+        Screening movingDayScreening = new Screening(1L, screeningDateTime, 1L, 1L);
+
+        PriceCalculator priceCalculator = new PriceCalculator();
+        int expectedPrice = 18000;
+
+        //when
+        int actualPrice = priceCalculator.applyDiscounts(basePrice, movingDayScreening);
+
+        //then
+        assertThat(actualPrice).isEqualTo(expectedPrice);
+    }
+
+    @Test
+    @DisplayName("오전 11시 이전 또는 오후 8시 이후에 시작하는 상영은 2,000원이 할인된다.")
+    void applyTimeDiscount(){
+        //given
+        int basePrice = 20000;
+        // 무비데이가 아닌 날, 할인 시간으로 설정
+        LocalDateTime screeningDateTime = LocalDateTime.of(2025, 9, 11, 10, 59);
+        Screening timeConditionScreening = new Screening(1L, screeningDateTime, 1L, 1L);
+
+        PriceCalculator priceCalculator = new PriceCalculator();
+        int expectedPrice = 18000;
+
+        //when
+        int actualPrice = priceCalculator.applyTimeDiscount(basePrice,  timeConditionScreening);
+
+        //then
+        assertThat(actualPrice).isEqualTo(expectedPrice);
+    }
+
+    @Test
+    @DisplayName("무비데이 할인과 시간 조건 할인 동시 적용 테스트")
+    void applyBothDiscountsMovindAndTime(){
+        //given
+        int basePrice = 20000;
+
+        LocalDateTime screeningDateTime = LocalDateTime.of(2025, 9, 20, 10, 59);
+        Screening bothScreening = new Screening(1L, screeningDateTime, 1L, 1L);
+
+        PriceCalculator priceCalculator = new PriceCalculator();
+        int expectedPrice = 16000;
+
+        //when
+        int actualPrice = priceCalculator.applyDiscounts(basePrice, bothScreening);
+
+        //then
+        assertThat(actualPrice).isEqualTo(expectedPrice);
+        System.out.println(actualPrice);
     }
 
 }
